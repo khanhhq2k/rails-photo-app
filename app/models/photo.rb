f4dd4412e8cefb50fa4e6ce3acac0796182facd8
Khanhnn <khanhhq2k@gmail.com>
2016-07-29 07:29:22 +0000

    add bootstrap for existing views

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :album
   validates :title, presence: true
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "900x900>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   paginates_per 8
   scope :public_photos, -> { where(public: true) }
